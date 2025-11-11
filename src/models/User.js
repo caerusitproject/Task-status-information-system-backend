d79bd38846950fd07cd9e163a684142b14129436
debanjansinha979-tech <debanjansinha979@gmail.com>
2025-11-11 18:20:50 +0530

    ..User change

--- src/models/User.js
+++ src/models/User.js
@@ -1,50 +1,51 @@
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../../config/db");
 
-const User = sequelize.define("users", {
+const User = sequelize.define(
+  "user",
+  {
     id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    auth_key: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // references: {
+      //   model: "Roles",
+      //   key: "name",
+      // },
+    },
   },
-  email_id: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  auth_key: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  role: {
-    type: DataTypes.STRING,
-     allowNull: false,
-    // references: {
-    //   model: "Roles",
-    //   key: "name",
-    // },
-  },
- 
-}, {
-  tableName: "users",
-  timestamps: false,
-  underscored: true,
-  freezeTableName: true
-});
-
-
+  {
+    tableName: "user",
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+  }
+);
 
 module.exports = User;
