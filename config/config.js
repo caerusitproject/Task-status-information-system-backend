dbf6d6ae8de99b24935a56442b37937bc92f1e13
Debanjan Sinha <debanjansinha979@gmail.com>
2025-12-03 13:16:09 +0530

    ...JWT_expires tinmelinie increased

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    logging: false
+    dialect: "postgres",
+    logging: false,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'secret',
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d'
-  }
+    secret: process.env.JWT_SECRET || "secret",
+    expiresIn: process.env.JWT_EXPIRES_IN || "365d",
+  },
 };
